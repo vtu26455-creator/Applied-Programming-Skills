2979dea892b1b2bc3b147cd14cdb561930f65a70
vtu26455-creator <vtu26455@veltech.edu.in>
2026-03-25 09:33:28 +0530

    Time: 24 ms (54.5%), Space: 76.1 MB (88%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // Check duplicate
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+
+            // Maintain window size k
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
